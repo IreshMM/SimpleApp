530b2ed2a692828908d25863a8858d347f1914e6
iresh <socialexpz1@gmail.com>
2020-01-13 07:22:17 +0000

    change

--- buildscript.sh
+++ buildscript.sh
@@ -1,4 +1,23 @@
-#!/bin/sh
+#!/bin/bash 
+
+# Setup environment for ACE toolkit
+. /opt/IBM/ace-11.0.0.7/server/bin/mqsiprofile
+
+APP_NAME=${PWD##*/}
+
+echo $APP_NAME
+
+SERVICE=Xvfb
+if ! pgrep -x "$SERVICE" >/dev/null 
+then
+	# Xvfb isn't running let's start it
+        Xvfb :99 &
+fi
+
+export DISPLAY=:99
+
+cd ..
+
+mqsicreatebar -data `pwd` -b $APP_NAME/mybar.bar -a $APP_NAME
 
 
-Xvfb :99 &
